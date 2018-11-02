d4df6cb8a9004f026f8824f69b9082cf03cdd96a
azsha <azsha@naver.com>
2018-11-02 23:48:03 +0900

    주석 삭제

--- AZHashtagTextViewExample/AZHashtagTextViewExample/ViewController.swift
+++ AZHashtagTextViewExample/AZHashtagTextViewExample/ViewController.swift
@@ -19,15 +19,8 @@ class ViewController: UIViewController, UITextViewDelegate {
         
         myTextView.resolveHashTags()
         myTextView.linkTextAttributes = [NSForegroundColorAttributeName: UIColor.red]
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func textView(_ textView: UITextView, shouldInteractWith url: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
         
         if let aztextView = textView as? AZHashtagTextView {
